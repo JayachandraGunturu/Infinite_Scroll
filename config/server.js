852f864bbbeb07418855332efbb7a6cac9376506
jayachandra gunturu <gunturujayachandra@gmail.com>
2020-11-13 00:25:17 +0530

    Init commit

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = {
   APPLICATION_ID: process.env.APPLICATION_ID || 'gR8Y16VeVELvYmEuDZGkKBZZI01qJL0k4yD79XAskk0',
   SECRET: process.env.SECRET || 'kbRcHwzZEbq6fJTHnD59H-X1UUTkR-j6upok1MXkEGg',
-  CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+  CALLBACK_URL: process.env.CALLBACK_URL || `http://localhost:${process.env.PORT}`
 };
